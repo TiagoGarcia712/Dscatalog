753450df38affad95f47959f5c20c5a2d971bd65
Tiago Garcia da Costa <tiago.costa@duotecnologia.com.br>
2022-08-03 15:34:13 -0300

    primeiro commit

---
+++ dscatalog/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.example.dscatalog.services;
+
+import com.example.dscatalog.entities.Category;
+import com.example.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
